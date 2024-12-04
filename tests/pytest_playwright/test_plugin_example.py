1ce5b47fe8e7f3179967d56fdb90412be9788a27
Alexei A <alexey.alexandrenko@gmail.com>
2024-12-04 14:50:46 +0100

    Adds tests with CLI commands

---
+++ tests/pytest_playwright/test_plugin_example.py
@@ -0,0 +1,14 @@
+import pytest
+from playwright.sync_api import Page, expect
+
+
+# pytest .\test_plugin_example.py -- headed --browser webkit --browser firefox --slowmo 300 --screenshot only-on-failure --base-url http://localhost:8000/
+
+def test_simple_plugin_example(page: Page):
+    page.goto('')
+
+    page.get_by_label('First name').fill('Sofi')
+    page.get_by_role('button', name='Register', exact=True).click()
+
+    warning = page.get_by_text('Valid last name is required')
+    expect(warning).not_to_be_visible()
--- tests/pytest_playwright/test_pw_decorators.py
+++ tests/pytest_playwright/test_pw_decorators.py
@@ -1,7 +1,7 @@
 import pytest
 from playwright.sync_api import Page, Browser
 
-args: dict = {'locale': 'es_ES', 'timezone_id': "Europe/Madrid"} # move to reuse
+args: dict = {'locale': 'es_ES', 'timezone_id': "Europe/Madrid"}  # move to reuse
 
 
 class TestThing:
@@ -17,5 +17,13 @@ class TestThing:
     @pytest.mark.browser_context_args(**args)
     def test_three(self, page: Page, browser: Browser):
         browser.new_context(**args)
+        assert page.evaluate("Intl.DateTimeFormat().resolvedOptions().timeZone()") == "Europe/Madrid"
+        assert page.evaluate("window.navigator.languages") == ["es-ES"]
+
+    @pytest.mark.parametrize('arg1', ['val1',
+                                      'val2'])
+    @pytest.mark.browser_context_args(**args)
+    @pytest.marker.smoke
+    def test_four(self, page: Page): # Will run twice
         assert page.evaluate("Intl.DateTimeFormat().resolvedOptions().timeZone()") == "Europe/Madrid"
         assert page.evaluate("window.navigator.languages") == ["es-ES"]
